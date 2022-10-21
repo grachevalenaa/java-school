c9e0bb9e5598da6a30a6478e2eb68045a6db73ce
Gracheva Alyona <grachevalenaa@gmail.com>
2022-10-22 00:32:08 +0300

    Оптимизировала З2

--- src/ru/croc/task2/Task2.java
+++ src/ru/croc/task2/Task2.java
@@ -1,38 +1,26 @@
 package ru.croc.task2;
 
 public class Task2 {
-    public static String output(long value, int counter) {
-        return String.format("%.1f", value / (Math.pow(1024, counter)));
-    }
     public static void main(String[] args) {
         // считываем с командной строки строку с входным числом
 
         long value = Long.parseLong(args[0]);  // конвертирует String в long
 
-        int counter = 0;  // величина, с помощью которой будут выводиться единицы измерения
+        int indexOfMetric = 0;  // индекс единицы измерения в массиве metrics
 
-        // Узнаем, сколько раз по 1024 помещается в числе и находим counter
+        String[] metrics = {" B", " KB", " MB", " GB", " TB"};
+
+        // Узнаем, сколько раз по 1024 помещается в числе и находим indexOfMetric
         long tempValue = value;
         while (tempValue >= 1024) {
-            ++counter;
-            tempValue /=1024;
+            if (indexOfMetric == metrics.length - 1) {
+                break;
+            }
+            ++indexOfMetric;
+            tempValue /= 1024;
         }
 
-        // ! вывод (если ввести числа 1024 и , например, 1027, то вывод будет одинаковым)
-        switch(counter) {
-            case 0 -> System.out.println(output(value, counter) + " B");
+        System.out.println(String.format("%.1f", value / Math.pow(1024, indexOfMetric)) + metrics[indexOfMetric]);
 
-            case 1 -> System.out.println(output(value, counter) + " KB");
-
-            case 2 -> System.out.println(output(value, counter) + " MB");
-
-            case 3 -> System.out.println(output(value, counter) + " GB");
-
-            case 4 -> System.out.println(output(value, counter) + " TB");
-
-            case 5 -> System.out.println(output(value, counter) + " PB");
-
-            case 6 -> System.out.println(output(value, counter) + " EB");
-        }
     }
 }
