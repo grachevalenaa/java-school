4c37e1052c262be58ee9a3f0e950d010e0655290
Gracheva Alyona <grachevalenaa@gmail.com>
2022-11-23 16:35:47 +0300

    Сделала З13

---
+++ src/ru/croc/task13/MovieRecommendSystem.java
@@ -0,0 +1,107 @@
+package ru.croc.task13;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class MovieRecommendSystem {
+
+    private static final String MOVIESPATH = "src/ru/croc/task13/movies.txt";
+
+    private static final String VIEWINGHISTORYPATH = "src/ru/croc/task13/viewingHistory.txt";
+
+    private static final Map<Integer, String> MOVIES;
+
+    private static List<Viewer> viewers = new ArrayList<>();
+
+    static {
+        File file = new File(MOVIESPATH);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+            scanner.useDelimiter(",| *\\r\\n");
+            MOVIES = new HashMap<>();
+            while (scanner.hasNextLine()) {
+                MOVIES.put(scanner.nextInt(), scanner.next());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            scanner.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        Viewer mainUser = new Viewer();
+        Scanner input = new Scanner(System.in);
+        mainUser.editViewingHistory(input.nextLine());
+
+        try {
+            viewers = getViewingHistoryFromFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        removeViewers(mainUser);
+        removeWatchedMovies(mainUser);
+        System.out.println(MOVIES.get(recommendCounter()));
+    }
+
+    private static void removeViewers(Viewer mainUser) {
+        Iterator<Viewer> iterator = viewers.iterator();
+        Viewer viewer = new Viewer();
+        while (iterator.hasNext()) {
+            viewer = iterator.next();
+            if (!viewer.isRightViewer(mainUser)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    private static ArrayList<Viewer> getViewingHistoryFromFile() throws IOException {
+        File file = new File(VIEWINGHISTORYPATH);
+        Scanner scanner = new Scanner(file);
+        ArrayList<Viewer> viewers = new ArrayList<>();
+        String newLine;
+        int newId;
+        Viewer viewer;
+        while (scanner.hasNextLine()) {
+            viewer = new Viewer();
+            viewer.editViewingHistory(scanner.nextLine());
+            viewers.add(viewer);
+        }
+        return viewers;
+    }
+
+    private static void removeWatchedMovies(Viewer mainUser) {
+        for (Viewer viewer: viewers) {
+            viewer.removeMovies(mainUser);
+        }
+    }
+
+    private static int recommendCounter() {
+        Map<Integer, Integer> recommendations = new HashMap<>();
+
+        for (Viewer viewer: viewers) {
+            for (Integer key: viewer.getViewingHistory().keySet()) {
+                if (recommendations.containsKey(key)) {
+                    recommendations.put(key, recommendations.get(key) + viewer.getViewingHistory().get(key));
+                } else {
+                    recommendations.put(key, viewer.getViewingHistory().get(key));
+                }
+            }
+        }
+
+        int max = 0;
+        int id = 0;
+        for(Integer key: recommendations.keySet()) {
+            if (recommendations.get(key) > max) {
+                max = recommendations.get(key);
+                id = key;
+            }
+        }
+
+        return id;
+    }
+
+}
---
+++ src/ru/croc/task13/Viewer.java
@@ -0,0 +1,47 @@
+package ru.croc.task13;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Viewer {
+
+    private Map<Integer, Integer> viewingHistory = new HashMap<>();  // key: id фильма, value: кол-во просмотров
+
+    public Map<Integer, Integer> getViewingHistory() {
+        return viewingHistory;
+    }
+
+    public void editViewingHistory(String history) {
+        // конвертирует из строки history вида "2,1,3" поле Map<Integer, Integer> viewingHistory
+        int newId;
+        for (String numString : history.split(",")) {
+            newId = Integer.parseInt(numString);
+            if (!viewingHistory.containsKey(newId)) {
+                viewingHistory.put(newId, 1);
+            } else {
+                viewingHistory.put(newId, viewingHistory.get(newId) + 1);
+            }
+        }
+    }
+
+    public boolean isRightViewer(Viewer mainUser) {
+        double matchCounter = 0;  // эта переменная double, так как в return matchCounter сравнивается с double
+        for (Integer key: mainUser.getViewingHistory().keySet()) {
+            if (viewingHistory.containsKey(key)) {
+                ++matchCounter;
+            }
+        }
+        return (matchCounter >= mainUser.viewingHistory.size() / 2.0);
+    }
+
+    public void removeMovies(Viewer mainUser) {
+        for (Integer key: mainUser.getViewingHistory().keySet()) {
+            if (viewingHistory.containsKey(key)) {
+                viewingHistory.remove(key);
+            }
+        }
+
+    }
+}
