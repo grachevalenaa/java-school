f41b3e7f3e5b2cd0cdd3ea0729a1abc495aab4f0
Gracheva Alyona <grachevalenaa@gmail.com>
2022-12-04 00:07:12 +0300

    Сделала З15

---
+++ src/ru/croc/task15/Task15.java
@@ -0,0 +1,46 @@
+package ru.croc.task15;
+
+import com.sun.source.tree.Tree;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.*;
+
+public class Task15 {
+    public static void main(String[] args) {
+        TreeNode root = convertDataFromFile();
+        HashSet<Integer> differentHours = new HashSet<>();
+        root.calculateTotalTime(differentHours);
+        System.out.println(differentHours);
+    }
+
+
+
+    private static final String DATAPATH = "src/ru/croc/task15/input.txt";
+
+    private static TreeNode convertDataFromFile() {
+        File file = new File(DATAPATH);
+        try (Scanner scanner = new Scanner(file)) {
+            scanner.useDelimiter(",| *\\r\\n");
+            String trash = scanner.next(); // trash - это костыль, туда помещается информация из файла, которую не использую
+            trash = scanner.next();
+            int time = scanner.nextInt();
+            TreeNode root = new TreeNode(time);
+            TreeNode parent;
+            while (scanner.hasNextLine()) {
+                trash = scanner.next();  // в trash помещается строка вида "C11"
+                parent = root.findNode(scanner.next());  // находим родителя по строке вида "B1"
+                time = scanner.nextInt();  // время обработки
+
+                parent.setChild(new TreeNode(parent, time + parent.getTime()));  // создаем новый node в дереве
+            }
+            return root;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ src/ru/croc/task15/TreeNode.java
@@ -0,0 +1,75 @@
+package ru.croc.task15;
+
+import java.util.*;
+
+public class TreeNode {
+
+    private final TreeNode parent;
+
+    private List<TreeNode> children;
+
+    // private Map<String, Integer> processingTime;
+
+    private final int processingTime;
+
+    /*public Set<Integer> getDifferentHours() {
+        return differentHours;
+    }*/
+
+    TreeNode(int processingTime) {
+        this.processingTime = processingTime;
+        this.parent = null;
+        this.children = new ArrayList<TreeNode>();
+    }
+
+    TreeNode(TreeNode parent, int processingTime) {
+        this.processingTime = processingTime;
+        this.parent = parent;
+        this.children = new ArrayList<TreeNode>();
+        // parent.setChild(this);
+    }
+
+    //private Set<Integer> differentHours;
+    /*{
+        differentHours = new HashSet<>();
+    }*/
+
+    public List<TreeNode> getChildren() {
+        return children;
+    }
+
+    public TreeNode getChildOf(int i) {
+        return children.get(i);
+    }
+
+    public int getTime() {
+        return processingTime;
+    }
+
+    public void setChild(TreeNode child) {
+        this.children.add(child);
+    }
+
+    public TreeNode findNode(String target) {
+        // итерирование по уровням дерева и нахождение node'а по строке вида "C112"
+        TreeNode node = this;
+        int index;
+        for (int i = 1; i < target.length(); ++i) {
+            index = Character.digit(target.charAt(i), 10) - 1;
+            node = node.getChildOf(index);
+        }
+        return node;
+    }
+
+
+    public void calculateTotalTime(HashSet<Integer> times) {
+        if (this.children.isEmpty()) {
+             times.add(this.processingTime);
+        }
+        for (TreeNode node: this.children) {
+            node.calculateTotalTime(times);
+        }
+    }
+
+
+}
