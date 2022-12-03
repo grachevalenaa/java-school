9f8b531a919e3173f124ca7273a6f32f057afcf6
Gracheva Alyona <grachevalenaa@gmail.com>
2022-12-03 23:00:43 +0300

    Сделала З14

---
+++ src/ru/croc/task14/BlackListFilter.java
@@ -0,0 +1,17 @@
+package ru.croc.task14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface BlackListFilter<E extends Iterable<T>, T> {
+
+    default List<T> toFilterComment(Iterable<T> in, Predicate<T> predicate) {
+        List<T> out = new ArrayList<>();
+        in.forEach(elem -> {
+            if (predicate.test(elem)) out.add(elem);
+        });
+        return out;
+    }
+
+}
