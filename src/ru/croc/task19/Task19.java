ab29724cbf6dbd0a8e43ac502112968dbc5fd117
Gracheva Alyona <grachevalenaa@gmail.com>
2022-12-07 16:32:33 +0300

    Сделала З19 ;)

---
+++ src/ru/croc/task19/Task19.java
@@ -0,0 +1,14 @@
+package ru.croc.task19;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Task19 {
+    public static void main(String[] args) throws IOException {
+        String str = "Hello, world!";
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src/ru/croc/task19/output.txt"));
+        writer.write(str);
+        writer.close();
+    }
+}
